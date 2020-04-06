33d22668b71e1f0bdab07eb53de30f61c3bd1672
Lei <gabriel_sl19798@hotmail.com>
2020-04-06 19:15:04 +1200

    mysql data connection

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,24 @@
 <?php
 
-echo "Hello World";
+$host = 'us-cdbr-iron-east-01.cleardb.net';
+$dbuser = 'b201689fe510f0';
+$dbpwd = '12117bab';
+$dbname = 'heroku_38954b5e03dd24b';
+
+$db = new mysqli($host, $dbuser, $dbpwd, $dbname);
+if ($db->connect_error){
+    echo $db->connect_error;
+}
+
+
+$sql = "select * from user";
+$res = $db->query($sql);
+
+if ($res->num_rows>0){
+    while ($row = $res->fetch_assoc()){
+        echo "<br>id: ".$row['id']."name: ".$row['name'];
+    }
+}
+
 
 ?>
