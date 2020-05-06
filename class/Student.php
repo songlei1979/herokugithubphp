ac2410905db8852f6a7e823469c1503803ebf9e7
Lei <gabriel_sl19798@hotmail.com>
2020-05-06 17:40:37 +1200

    student class and api for register check error

--- class/Student.php
+++ class/Student.php
@@ -22,6 +22,7 @@ class Student
         //if I don't have this object in my database, I will register him first
         if (is_null($this->id)){
             $query = "insert into student values (null, '$this->name', 'this->username', 'this->password')";
+            echo $query;
             mysqli_query($this->dbconn, $query);
         }else{
             $query = "Update student SET name = '$this->name' where id = $this->id";
