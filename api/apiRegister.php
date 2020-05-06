55fcf5f456845c8e1d4212f83fa2420aa96ec07a
Lei <gabriel_sl19798@hotmail.com>
2020-05-06 17:42:19 +1200

    student class and api for register check error 1

--- api/apiRegister.php
+++ api/apiRegister.php
@@ -3,6 +3,9 @@
 if (isset($_POST["name"])){
     include_once "../class/Student.php";
     $student = new Student(null, $_POST["name"], $_POST["username"], $_POST["password"]);
+    echo $_POST["name"];
+    echo $_POST["username"];
+    echo $_POST["password"];
     $student->save();
     $msg = "new user has been added";
 }else{
