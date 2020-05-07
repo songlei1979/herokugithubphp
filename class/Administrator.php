455f985ff92c37b91aba19bbc6211e62d266c375
Lei <gabriel_sl19798@hotmail.com>
2020-05-07 17:45:22 +1200

    login form ajas

--- class/Administrator.php
+++ class/Administrator.php
@@ -17,8 +17,8 @@ class Administrator
     public function login($username, $password){
         //1. I check his username
         $query = "select * from administrator where username = '$username'";
-        echo $query;
-        $result = mysqli_query($this->dbconn, $query);
+        //echo $query;
+        $result = mysqli_query($this->conn, $query);
         if ($result->num_rows == 1){ //2. if there is a record, I will check his password
             while ($row = $result->fetch_assoc()){
                 if ($row['password'] == $password){
