84527f02947a9f54d3b75fe829ff08178d57ba65
Lei <gabriel_sl19798@hotmail.com>
2020-05-07 17:03:29 +1200

    admin login form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
+    $("#loginBtn").click(function () {
+        posting = $.post( url, { username: $('#username').val(), password: $('#password').val() } );
+        posting.done(function( data ) {
+            alert('success');
+            $("#loginDiv").hide();
+        });
+    }) ;
+
+
 
 });
