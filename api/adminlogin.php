0237bcd1dde456aef289f1bba32ae1e19a2fb1ed
Lei <gabriel_sl19798@hotmail.com>
2020-05-07 17:11:54 +1200

    login form ajas

--- api/adminlogin.php
+++ api/adminlogin.php
@@ -4,12 +4,14 @@ if (isset($_POST['username'])){
     $username = $_POST['username'];
     $pwd = $_POST['password'];
     $admin = new Administrator();
-    $admin->login();
-    if (!is_null($admin->id )){
-        session_start();
-        $_SESSION['admin_id'] = $admin->id;
-        echo "<p><a href='liststudents.php'>List students</a></p>";
-    }
+    echo $username;
+    echo $pwd;
+//    $admin->login();
+//    if (!is_null($admin->id )){
+//        session_start();
+//        $_SESSION['admin_id'] = $admin->id;
+//        echo "<p><a href='liststudents.php'>List students</a></p>";
+//    }
 
 }else{
     $msg = json_encode("wrong login");
