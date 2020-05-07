60e3d88187878637471a36521de0f2b127685e38
Lei <gabriel_sl19798@hotmail.com>
2020-05-07 18:46:13 +1200

    login form ajas

--- class/Administrator.php
+++ class/Administrator.php
@@ -36,9 +36,9 @@ class Administrator
         $result = mysqli_query($this->conn, $query);
         if ($result->num_rows > 0){
             $students = array(); //for transfer data, I use objects array
-            while ($row = $result->fetch_row()){
+            while ($row = $result->fetch_assoc()){
                 $student = new Student($row['id'], $row['name'], $row['username'], $row['password']);
-                $student = new Student(1, '1', '1', '1');
+                //$student = new Student(1, '1', '1', '1');
                 array_push($students, $student);
             }
             return $students;
