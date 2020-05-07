2fd210d5bcf472aacdb905bcb7aa25d2217a2705
Lei <gabriel_sl19798@hotmail.com>
2020-05-07 17:04:47 +1200

    login form ajas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $("#loginBtn").click(function () {
+        url = "http://herokugitphpleisong.herokuapp.com/api/adminlogin.php";
         posting = $.post( url, { username: $('#username').val(), password: $('#password').val() } );
         posting.done(function( data ) {
             alert('success');
