fb7e955be54da5439a8b1039ca4e90ad81bd5775
Lei <gabriel_sl19798@hotmail.com>
2020-05-11 14:46:27 +1200

    show single student

--- class/Administrator.php
+++ class/Administrator.php
@@ -49,6 +49,7 @@ class Administrator
 
     public function showStudent($id){
         $query = "select * from student where id="+$id;
+        echo $query;
         $result = mysqli_query($this->conn, $query);
         if ($result->num_rows > 0) {
             while ($row = $result->fetch_assoc()){
