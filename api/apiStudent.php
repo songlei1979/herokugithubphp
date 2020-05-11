859871adaa14287b3d16aa798b0c2465324ec76d
Lei <gabriel_sl19798@hotmail.com>
2020-05-11 14:58:15 +1200

    show single student

--- api/apiStudent.php
+++ api/apiStudent.php
@@ -3,7 +3,7 @@
 //if (isset($_SESSION['admin_id'])) {
     include_once "../class/Administrator.php";
     $admin = new Administrator();
-    //$student = $admin->showStudent($_GET["id"]);
+    $student = $admin->showStudent($_GET["id"]);
     //echo json_encode($student);
 //}else{
 //    echo json_encode("login please");
